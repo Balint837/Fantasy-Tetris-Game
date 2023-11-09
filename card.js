803895b5c0912fc9cefa1293b1f4d7aef6d1b604
csiguszka <szabo.taddeus@students.jedlik.eu>
2023-11-09 09:38:17 +0100

    tetrsi rnd shape

--- card.js
+++ card.js
@@ -1,4 +1,5 @@
 import { CardField } from "./CardField.js"
+import { tetrisShapes } from "./shapes.js"
 
 export class Card{
         constructor(index, cards){
@@ -14,32 +15,20 @@ export class Card{
           const cardDiv = document.createElement("div")
           cardDiv.classList.add("cardDiv")
           cardDiv.addEventListener('click', () => {
-            this.Select();
+            this.#Select();
           });
 
-          this.CreateCardShape()
-          cardDiv.innerHTML +=`
-          <img src="card/card.png" alt="Card" class="card">
-          <p class="cardText">sdfsdfjksdfjksdfjk</p>`
-          this.cardFields.forEach(cardField => {
-            cardDiv.innerHTML +=`
-          <div class="cardField"></div>
-            `
-          });
-          cardDiv.innerHTML +=`
-          <svg class="cardTitle" viewBox="0 0 500 500">
-              <path id="curve" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97"/>
-              <text width="500">
-                <textPath xlink:href="#curve">
-                  Dangerous Vala
-                </textPath>
-              </text>
-          </svg>
-          `
+          const random = Math.floor(Math.random() * tetrisShapes.length);
+          console.log(random, tetrisShapes[random]);
+
+          cardDiv.innerHTML += this.#AddImgAndTexts()
+          cardDiv.innerHTML += this.#AddTable(tetrisShapes[random])
+
           cardsDiv.appendChild(cardDiv)
       }
     
-      Select(){
+      #Select(){
+
         this.cards.forEach(card => {
           if (card == this){
             card.isSelect = true
@@ -51,50 +40,37 @@ export class Card{
             cardElement.id = ""
           }
         });
+
+      }
+      #AddImgAndTexts(){
+        return `
+        <img src="card/card.png" alt="Card" class="card">
+        <p class="cardText">sdfsdfjksdfjksdfjk</p>
+        <svg class="cardTitle" viewBox="0 0 500 500">
+            <path id="curve" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97"/>
+            <text width="500">
+              <textPath xlink:href="#curve">
+                Dangerous Vala
+              </textPath>
+            </text>
+        </svg>
+        `
       }
 
-      CreateCardShape(){
-        this.cardFields.push(new CardField(0, 0))
-        let currentPos = [0, 0]
-        while (this.cardFields.length != 5){
-            if (Math.random()>0.3){
-              while (true){
-                const direction = parseInt(Math.random()*100)/4
-                let IsOk
-                if (direction <= 25){
-                  IsOk = this.CheckPos(currentPos[0], currentPos[1] + 1)
-                  currentPos = [currentPos[0], currentPos[0] + 1]
-                }
-                else if (direction <= 50){
-                  IsOk = this.CheckPos(currentPos[0], currentPos[1] - 1)
-                  currentPos = [currentPos[0], currentPos[0] - 1]
-                }
-                else if (direction <= 75){
-                  IsOk = this.CheckPos(currentPos[0] + 1, currentPos[1])
-                  currentPos = [currentPos[0] + 1, currentPos[0]]
-                }
-                else{
-                  IsOk = this.CheckPos(currentPos[0] - 1, currentPos[1])
-                  currentPos = [currentPos[0] - 1, currentPos[0]]
-                }
-                if (IsOk){
-                  break
-                }
+      #AddTable(matrix){
+        let result = "<table class=\"cardTable\">"
+        matrix.forEach(x => {
+          result += "<tr class=\"cardTr\">"
+            x.forEach(i =>{
+              if (x[i] === 1){
+                result += "<td class=\"cardTd red\"></td>"
+              }else{
+                result += "<td class=\"cardTd\"></td>"
               }
-            }
-            else{
-              break
-            }
-        }
-      }
-
-      CheckPos(x, y){
-        this.cardFields.forEach(cardField => {
-          if (cardField.relX == x && cardField.relY == y){
-            return false
-          }
-        });
-        this.cardFields.push(new CardField(x, y))
-        return true
+            })
+            result += "</tr>"
+          });
+        result += "</table>"
+        return result 
       }
 }
