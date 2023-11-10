986135a50cab442af42de9bba346abe580d20d8e
Balint817 <pecsibalint2@gmail.com>
2023-11-10 09:15:59 +0100

    added table

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
 import { Card } from "./card.js";
+
+function OnResize() {
+    console.log("fired OnResize");
+    UpdateTableSize();
+}
+
+function UpdateTableSize() {
+    
+    
+    let height = document.documentElement.clientHeight;
+    let width = document.documentElement.clientWidth;
+
+    console.log(height, width);
+    height = height * 0.9;
+    width = width * 0.45;
+
+    height = height / document.querySelectorAll("#main-table > tbody > tr").length
+    width = width / document.querySelectorAll("#main-table > tbody > tr:first-child > td").length
+
+    console.log(height, width);
+    const min = Math.min(height, width);
+    let squareSize = `${min}px`
+    for (const element of document.querySelectorAll("#main-table > tbody > tr > td")) {
+        element.style.width = squareSize;
+        element.style.height = squareSize;
+    }
+}
+
+
 const backgroundCard = document.querySelector(".backgroundCard")
 let cards = []
 
@@ -7,3 +36,6 @@ backgroundCard.addEventListener('click', function() {
     cards.push(new Card(1, cards))
     cards.push(new Card(2, cards))
   });
+
+window.addEventListener('resize', OnResize)
+OnResize(); // initial call;
